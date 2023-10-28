92860a3787607b02b86fbb807e3eee378cd45677
Naoki_re <92578808+NaokiRe@users.noreply.github.com>
2023-10-28 22:26:11 +0100

    completed book project

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+export const PORT = 5555;
+export const mongoDBURL =
+    "mongodb+srv://billynaoki0604:1M7RomJML7QnTkSK@booksystemtutorial.xkzvtxi.mongodb.net/books-collection?retryWrites=true&w=majority"
--- config.js
+++
@@ -1,3 +0,0 @@
-export const PORT = 5555;
-export const mongoDBURL = 
-    "mongodb+srv://paytonliao:1211payton@cluster0.joydphw.mongodb.net/books-collection?retryWrites=true&w=majority"
